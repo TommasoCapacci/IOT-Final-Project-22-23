4df9463dcc9ba14d331e43516ed71599e56c7557
Tommaso Capacci <tommasocapacci@gmail.com>
2023-07-23 11:51:21 +0200

    started implementation (copy of template)

---
+++ code/RadioRoute.h
@@ -0,0 +1,18 @@
+
+
+#ifndef RADIO_ROUTE_H
+#define RADIO_ROUTE_H
+
+typedef nx_struct radio_route_msg {
+	//field 1
+	//field 2
+	//field 3
+	//....
+	//field n
+} radio_route_msg_t;
+
+enum {
+  AM_RADIO_COUNT_MSG = 10,
+};
+
+#endif
